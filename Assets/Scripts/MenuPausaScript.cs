a4d5b82023447f5eac808ee1eb9f84dbe29044b9
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-05-30 16:41:38 -0500

    Menu de Pausa

---
+++ Assets/Scripts/MenuPausaScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausaScript : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(juegoPausado) Reanudar();
+            else Pausa();
+        }
+    }
+    public void Pausa(){
+        juegoPausado = true;
+        Time.timeScale = 0;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+    public void Reanudar(){
+        juegoPausado = false;
+        Time.timeScale = 1;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+    public void Reiniciar(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
