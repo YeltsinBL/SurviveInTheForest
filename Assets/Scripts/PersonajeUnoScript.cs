2d4bc423165b958a324a939c3a4a9751ecc43f70
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-05-30 17:17:49 -0500

    Agregar salto doble al personaje 1

--- Assets/Scripts/PersonajeUnoScript.cs
+++ Assets/Scripts/PersonajeUnoScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,9 @@ public class PersonajeUnoScript : MonoBehaviour
     public float jumpForce; // fuerza para saltar
     private bool grounded; // verifica si estamos en el suelo
     private bool mirandoDerecha;
+    private bool salto=false;
+    [SerializeField] private int saltosExtrasRestantes;
+    [SerializeField] private int saltosExtras;
 
     // Start is called before the first frame update
     void Start()
@@ -35,9 +39,11 @@ public class PersonajeUnoScript : MonoBehaviour
         if(Physics2D.Raycast(transform.position, Vector3.down, 0.6f)) grounded=true;
         else grounded=false;
 
+        if(grounded) saltosExtrasRestantes = saltosExtras;
         // agregar la tecla espacio para saltar
-        if(Input.GetKeyDown(KeyCode.Space) && grounded) {
-            Jump();
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            salto=true;
+            //Jump();
         } 
         if(!grounded) animator.SetBool("Jumping",true);
         else animator.SetBool("Jumping",false);
@@ -46,11 +52,28 @@ public class PersonajeUnoScript : MonoBehaviour
     // Mover al personaje
     private void FixedUpdate() {
         rigidbody2D.velocity = new Vector2(horizontal * speed, rigidbody2D.velocity.y);
+        Movimiento(salto);
+        salto = false;
+    }
+
+    private void Movimiento(bool salto)
+    {
+        if(salto){
+            if(grounded){
+                Jump();
+            }else{
+                if(salto && saltosExtrasRestantes>0){
+                    Jump();
+                    saltosExtrasRestantes -=1;
+                }
+            }
+        }
     }
 
     // Saltar
     private void Jump(){
         rigidbody2D.AddForce(Vector2.up * jumpForce);
+        salto=false;
     }
     
     private void Girar(){
